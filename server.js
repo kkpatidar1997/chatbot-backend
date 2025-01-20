0da947e78d932ffedf8f667d3f1df6bc260cf50b
kkpatidar1997 <kkpatidar1997@gmail.com>
2025-01-20 06:57:53 +0530

    added port

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 const app = express();
-const port = 5000;
+
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
